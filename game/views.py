9cb0adf81b58c9e45cc31977196f4f325596a640
Divyanshi <divyanshigautam9922@gmail.com>
2026-05-11 19:55:23 +0530

    add actual reason of draw to the database
    q
    Q

--- game/views.py
+++ game/views.py
@@ -71,7 +71,8 @@ def make_move(request):
             winner = 'black' if game.current_turn == 'white' else 'white'
             record_game_result(game.mode, winner, 'checkmate')
         elif game_status in ('stalemate', 'draw'):
-            record_game_result(game.mode, 'draw', 'stalemate')
+            actual_reason = game.draw_reason or 'stalemate'
+            record_game_result(game.mode, 'draw', actual_reason)  # Correct reason!
 
     return JsonResponse({
         'valid': success,
